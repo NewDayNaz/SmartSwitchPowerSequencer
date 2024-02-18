92171b40458ce9a8ec9f20397cd2a72544bb02c3
NewDayNaz <mgraham@newdaynaz.org>
2024-02-18 09:39:54 -0600

    Fixed not pulling label from device alias if not set in config

--- devices.py
+++ devices.py
@@ -23,23 +23,26 @@ class GenericDevice:
 class KasaSmartSwitch(GenericDevice):
     def __init__(self, label=None, ip=None, predelay=0, show_status=True):
         super().__init__(label, ip, predelay, show_status)
+        self.plug = SmartPlug(self.ip) if self.ip != "all" else None
+        if self.plug is not None:
+            self.label = self.plug.alias
 
     def turn_off(self):
         time.sleep(self.predelay)
-        plug = SmartPlug(self.ip)
-        plug.turn_off()
-        plug = None
+        if self.plug is not None:
+            self.plug.turn_off()
         return True
 
     def turn_on(self):
         time.sleep(self.predelay)
-        plug = SmartPlug(self.ip)
-        plug.turn_on()
-        plug = None
+        if self.plug is not None:
+            self.plug.turn_on()
         return True
 
     def status(self):
-        plug = SmartPlug(self.ip) if self.ip != "all" else None
-        status_str = "(%s)" % plug.state if plug else "UNAVAILABLE (OFFLINE)"
-        plug = None
+        if self.plug is not None:
+            status_str = "(%s)" % plug.state if plug else "UNAVAILABLE (OFFLINE)"
         return status_str
+    
+    def __del__(self):
+        self.plug = None
